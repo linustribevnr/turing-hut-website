8ab2417a8913fc2acdbfc13fe351764a8257e9bb
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2022-09-10 16:34:39 +0530

    fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,36 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        "name": "images",
-        "path": "./src/images/"
-      },
-      __key: "images"
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        "name": "pages",
-        "path": "./src/pages/"
-      },
-      __key: "pages"
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `limelight`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
-        ],
-        display: "swap"
-      }
-    }
-  ]
+  plugins: []
 };
