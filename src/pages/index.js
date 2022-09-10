e66676e365ffdd4d837e79a5f446d0d466b434cf
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2022-09-10 17:39:39 +0530

    use CamelCase for component file names

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from "../components/layout";
-import Test from "../components/test";
+import Layout from "../components/Layout";
+import Test from "../components/Test";
 function indexPage() {
   return (
     <Layout>
