6243ae8eeaef64710aeb75146162eb4679d36115
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2022-09-10 17:40:50 +0530

    rename test to Test component

--- src/components/test.js
+++ src/components/test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 import HomeIcon from "@mui/icons-material/Home";
 
-export default function test() {
+export default function Test() {
   return (
     <Button variant="contained">
       <HomeIcon /> Hello World
