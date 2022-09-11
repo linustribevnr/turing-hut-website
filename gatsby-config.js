fd53f335cadc508ad810be5f00a5bd500dee4efb
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2022-09-11 18:33:35 +0530

    install plugins needed for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: []
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      }
+    }
+  ]
 };
