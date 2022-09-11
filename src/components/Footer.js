3e062127e7c9e7f552b04c7c297bc407426f9316
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2022-09-11 20:54:58 +0530

    feat: footer

---
+++ src/components/Footer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+    Box,
+    Divider, Typography, Toolbar, Tooltip, Grid
+  } from "@mui/material";
+  import { Link } from "gatsby";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+
+const socialMediaItems = [
+    {
+        name: 'GitHub',
+        icon: <GitHubIcon/>,
+        link: "https://github.com/linustribevnr"
+    },
+    {
+        name: 'YouTube',
+        icon: <YouTubeIcon/>,
+        link: "https://www.youtube.com/channel/UCBJz-RDa3d4rTLycV_MhTPA"
+    },
+    {
+        name: 'Instagram',
+        icon: <InstagramIcon/>,
+        link: "https://instagram.com/turing.hut"
+    },
+    {
+        name: 'LinkedIn',
+        icon: <LinkedInIcon/>,
+        link: "https://www.linkedin.com/company/turinghut"
+    }
+];
+
+export default function Footer() {
+  return (
+    <Box sx={{my:2}}>
+        <Divider/>
+        <Grid container spacing={0}>
+            <Grid item xs={12} md={9} sx={{my:2, textAlign: {xs: 'center', md: 'left'}}}>
+                <Typography variant="body2" component="div" sx={{ flexGrow: 1, color: "#757575" }}>“The most damaging phrase in the language is.. it's always been done this way” - Grace Hopper</Typography>
+            </Grid>
+            <Grid item xs={12} md={3} sx={{my:2}}>
+                <Box sx={{ textAlign: 'center'}}>
+                    {socialMediaItems.map(item => (
+                        <Link
+                            key={item.link}
+                            to={item.link}
+                            target='_blank'
+                            style={{ textDecoration: "none", color: "#757575", padding: "10px"}}
+                        >
+                            <Tooltip title={item.name} placement="top">
+                                {item.icon}
+                            </Tooltip>
+                        </Link>
+                    ))}        
+                </Box>
+            </Grid>
+        </Grid>
+    </Box>
+  )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import { Box, Toolbar } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { green } from "@mui/material/colors";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const theme = createTheme({
   palette: {
@@ -15,10 +16,10 @@ export default function Layout({ children }) {
     <ThemeProvider theme={theme}>
       <Box sx={{ display: "flex" }}>
         <Navbar />
-        <Box component="main" sx={{ p: 3 }}>
+        <Box component="main" sx={{ p: 3, width:1 }}>
           <Toolbar />
           {children}
-          <div>Footer</div>
+          <Footer />
         </Box>
       </Box>
     </ThemeProvider>
