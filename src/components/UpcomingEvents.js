a834a81b8f2b51e6533f3bf47cb2da75310d188c
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2022-09-14 19:38:06 +0530

    remove content of upcomingevents

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -4,14 +4,14 @@ import { Grid, Box, Typography } from "@mui/material";
 export default function UpcomingEvents() {
   return (
     <Box sx={{ bgcolor: "#DEDEDE" }}>
-      <Typography variant="h5" color="initial">
+      {/* <Typography variant="h5" color="initial">
         Upcoming Events.
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
           hello
         </Grid>
-      </Grid>
+      </Grid> */}
     </Box>
   );
 }
