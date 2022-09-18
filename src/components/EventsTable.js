8d80e3f765d06430b8a60ea657267ef49d073738
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2022-09-18 20:57:11 +0530

    sort and pagination added

--- src/components/EventsTable.js
+++ src/components/EventsTable.js
@@ -7,13 +7,23 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TableSortLabel,
   Typography,
   Paper,
+  TablePagination,
   TextField
 } from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
+
 import { Link, graphql, useStaticQuery } from "gatsby";
 
 export default function EventsTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [search, setSearch] = useState("");
+  const [order, setOrder] = useState("");
+  const [orderBy, setOrderBy] = useState("");
+  const header = ["Name", "Date", "Type"];
   const data = useStaticQuery(graphql`
     query Events {
       allMarkdownRemark {
@@ -39,7 +49,30 @@ export default function EventsTable() {
       slug: event.frontmatter.slug
     };
   });
-  const [search, setSearch] = useState("");
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const createSortHandler = property => event => {
+    setOrderBy(property);
+    setOrder(order === "asc" ? "desc" : "asc");
+  };
+
+  function getSortedEvents(events) {
+    if (orderBy === "") return events;
+    return events.sort((x, y) => {
+      const a = x[orderBy.toLowerCase()];
+      const b = y[orderBy.toLowerCase()];
+      if (a > b) return order === "asc" ? -1 : 1;
+      else if (a < b) return order === "asc" ? 1 : -1;
+      return 0;
+    });
+  }
 
   return (
     <Box sx={{ my: 4 }}>
@@ -66,33 +99,63 @@ export default function EventsTable() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Event name</TableCell>
-              <TableCell align="right">Date</TableCell>
-              <TableCell align="right">Type</TableCell>
+              {header.map((val, i) => (
+                <TableCell
+                  key={i}
+                  align={["Date", "Type"].includes(val) ? "right" : "left"}
+                  onClick={createSortHandler(val)}
+                  sortDirection={orderBy === val ? order : false}>
+                  <TableSortLabel
+                    active={orderBy === val}
+                    direction={orderBy === val ? order : "asc"}>
+                    {val}
+                    {orderBy === val ? (
+                      <Box component="span" sx={visuallyHidden}>
+                        {order === "desc"
+                          ? "sorted descending"
+                          : "sorted ascending"}
+                      </Box>
+                    ) : null}
+                  </TableSortLabel>
+                </TableCell>
+              ))}
               <TableCell align="right">See More</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {events.map(
-              event =>
-                event.name.toLowerCase().includes(search) && (
-                  <TableRow
-                    key={event.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                    <TableCell component="th" scope="row">
-                      {event.name}
-                    </TableCell>
-                    <TableCell align="right">{event.date}</TableCell>
-                    <TableCell align="right">{event.type}</TableCell>
-                    <TableCell align="right">
-                      <Link to={`/events/${event.slug}`}>See more</Link>
-                    </TableCell>
-                  </TableRow>
-                )
-            )}
+            {getSortedEvents(events)
+              .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+              .map(
+                event =>
+                  event.name.toLowerCase().includes(search) && (
+                    <TableRow
+                      key={event.id}
+                      sx={{
+                        "&:last-child td, &:last-child th": { border: 0 }
+                      }}>
+                      <TableCell component="th" scope="row">
+                        {event.name}
+                      </TableCell>
+                      <TableCell align="right">{event.date}</TableCell>
+                      <TableCell align="right">{event.type}</TableCell>
+                      <TableCell align="right">
+                        <Link to={`/events/${event.slug}`}>See more</Link>
+                      </TableCell>
+                    </TableRow>
+                  )
+              )}
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 15]}
+        component="div"
+        count={events.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </Box>
   );
 }
