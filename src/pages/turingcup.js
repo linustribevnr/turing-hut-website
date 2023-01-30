8552015cb68323dc20da1ded8349d63352d3c0ed
Gautham Mallipeddi <gautham.mallipeddi8@gmail.com>
2023-01-30 21:07:47 +0530

    Added Turing Cup route

---
+++ src/pages/turingcup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Seo} from "../components/Seo"
+export const Head = () => (
+    <Seo routename={"turingcup"}/>
+)
+
+function turingcup() {
+return (
+    <div>turingcup</div>
+)
+}
+
+export default turingcup
