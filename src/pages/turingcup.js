6b14f60be916570d00e935e472d0e816435c32cd
srilasyakompella <srilasyakompella@gmail.com>
2023-02-18 23:37:24 +0530

    added collapse components

--- src/pages/turingcup.js
+++ src/pages/turingcup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Seo} from "../components/Seo"
 import LandingPage from '../components/TuringCup/LandingPage'
+import PastContests from '../components/TuringCup/PastContests'
 
 export const Head = () => (
     <Seo routename={"turingcup"}/>
@@ -11,6 +12,7 @@ return (
     <div>
         <h1>Turing Cup</h1>
         <LandingPage />
+        <PastContests />
     </div>
 )
 }
