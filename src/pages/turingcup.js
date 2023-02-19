7a47e939cdea0e2431164a3db861f9aa675c221a
Tejeswara Murthy palwadi <tejeswarmurthypalwadi@gmail.com>
2023-02-19 18:58:30 +0530

    created css file

--- src/pages/turingcup.js
+++ src/pages/turingcup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import {Seo} from "../components/Seo"
+import '../components/TuringCup/turingcup.css';
 import LandingPage from '../components/TuringCup/LandingPage';
 import PastContests from '../components/TuringCup/PastContests';
 import Timers from '../components/TuringCup/Timers';
@@ -15,11 +16,26 @@ export const Head = () => (
 function turingcup() {
 return (
   <Layout>
-    <LandingPage/>
-    <Timers/>
-    <TuringCupPrizePool/>
-    <TuringCupSponsors/>
-    <PastContests/>
+    
+        <div className="geometric-container">
+            <LandingPage className="infoo"/>
+            <Timers className="infoo"/>
+            <TuringCupPrizePool className="infoo"/>
+            <TuringCupSponsors className="infoo"/>
+             <PastContests className="infoo"/>
+            <div className="geometric-form child-1"></div>
+            <div className="geometric-form child-2"></div>
+            <div className="geometric-form child-3"></div>
+            <div className="geometric-form child-4"></div>
+            <div className="geometric-form child-5"></div>
+            <div className="geometric-form child-6"></div>
+            <div className="geometric-form child-7"></div>
+            <div className="geometric-form child-8"></div>
+            <div className="geometric-form child-9"></div>
+            <div className="geometric-form child-10"></div>
+            <div className="geometric-form child-11"></div>
+        </div>
+    
   </Layout>
 )
 }
