4d360707161edbbc231b497da0802dbf1e7e74ca
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2023-02-23 12:20:10 +0530

    fix: minor

--- src/components/TuringCup/Landingpage2.js
+++ src/components/TuringCup/Landingpage2.js
@@ -1,23 +1,95 @@
-import { Grid } from '@mui/material'
-import React from 'react'
-import './LandingPage2.css'
+import { Button, Grid, Stack } from "@mui/material";
+import HowToRegRoundedIcon from "@mui/icons-material/HowToRegRounded";
+import InsertInvitationRoundedIcon from "@mui/icons-material/InsertInvitationRounded";
+// npm install add-to-calendar-button to avail features of add to calendar.
+import { atcb_action } from "add-to-calendar-button";
+
+import React from "react";
+import "./LandingPage2.css";
 function Landingpage2() {
+  const config = {
+    name: "[Reminder] Turing Cup 2K23 Round - 1",
+    description: "Check your mail for the Contest Links",
+    startDate: "2023-03-11",
+    startTime: "19:30",
+    endTime: "22:00",
+    options: ["Google", "Apple", "Outlook.com"],
+    timeZone: "Asia/Calcutta"
+  };
+
+  const handleClick = () => {
+    atcb_action(config, document.getElementById("add-to-calendar-btn"));
+  };
+
   return (
-    <Grid className='fullpage' style={{fontFamily: "'Gruppo', cursive"}}>
-        <svg viewBox='0 0 1300 300'>
-            <text x="50%" y="70%" fill='transparent' textAnchor='middle' className='bigh' >
-                TURING  CUP
-            </text>
-        </svg>
-        <svg viewBox='0 0 1300 200'>
-            <text x="50%" y="26%" fill='transparent' textAnchor='middle' className='smallh' >
-                ONE TEAM , ONE DREAM!
-            </text>
-        </svg>
-        
-        {/* <h1>hi</h1> */}
+    <Grid
+      className="fullpage"
+      style={{ fontFamily: "'Gruppo', cursive", marginBottom: "16px" }}>
+      <svg viewBox="0 0 1300 300">
+        <text
+          x="50%"
+          y="70%"
+          fill="transparent"
+          textAnchor="middle"
+          className="bigh">
+          TURING CUP
+        </text>
+      </svg>
+      <svg viewBox="0 0 1300 200">
+        <text
+          x="50%"
+          y="26%"
+          fill="transparent"
+          textAnchor="middle"
+          className="smallh">
+          ONE TEAM , ONE DREAM!
+        </text>
+      </svg>
+
+      {/* <h1>hi</h1> */}
+      <Stack
+        style={{ marginTop: "-40px" }}
+        sx={{ p: 5, justifyContent: "center" }}
+        direction={{ xs: "column", md: "row" }}
+        spacing={2}>
+        <Button
+          sx={{
+            fontFamily: ['"Gruppo"', "cursive"].join(","),
+            backgroundColor: "#101820FF",
+            color: "#F2AA4CFF",
+            border: "1px solid #F2AA4CFF",
+            "&:hover": {
+              backgroundColor: "#F2AA4CFF",
+              color: "#fff"
+            }
+          }}
+          variant="contained"
+          href="https://unstop.com/o/ue5gaLR?lb=Wqrocjv"
+          target="__blank"
+          startIcon={<HowToRegRoundedIcon />}>
+          Register Now
+        </Button>
+
+        <Button
+          sx={{
+            fontFamily: ['"Gruppo"', "cursive"].join(","),
+            backgroundColor: "#101820FF",
+            color: "#F2AA4CFF",
+            border: "1px solid #F2AA4CFF",
+            "&:hover": {
+              backgroundColor: "#F2AA4CFF",
+              color: "#fff"
+            }
+          }}
+          id="add-to-calendar-btn"
+          onClick={handleClick}
+          variant="contained"
+          endIcon={<InsertInvitationRoundedIcon />}>
+          Add to Calendar
+        </Button>
+      </Stack>
     </Grid>
-  )
+  );
 }
 
-export default Landingpage2
+export default Landingpage2;
--- src/pages/turingcup.js
+++ src/pages/turingcup.js
@@ -1,49 +1,55 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import {Seo} from "../components/Seo"
-import '../components/TuringCup/turingcup.css';
-import PastContests from '../components/TuringCup/PastContests';
-import Timers from '../components/TuringCup/Timers';
-import TuringCupPrizePool from '../components/TuringCup/TuringCupPrizePool';
+import React from "react";
+import Layout from "../components/Layout";
+import { Seo } from "../components/Seo";
+import "../components/TuringCup/turingcup.css";
+import PastContests from "../components/TuringCup/PastContests";
+import Timers from "../components/TuringCup/Timers";
+import TuringCupPrizePool from "../components/TuringCup/TuringCupPrizePool";
 // import TuringCupSponsors from '../components/TuringCup/TuringCupSponsors';
 import { Box } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import Landingpage2 from '../components/TuringCup/Landingpage2';
-import Timelinee from '../components/TuringCup/Timelinee';
-import TuringCupSponsors from '../components/TuringCup/TuringCupSponsors';
-import '../styles/global.css';
+import Landingpage2 from "../components/TuringCup/Landingpage2";
+import Timelinee from "../components/TuringCup/Timelinee";
+import TuringCupSponsors from "../components/TuringCup/TuringCupSponsors";
 
-import Faqs from '../components/TuringCup/Faqs'
+import Faqs from "../components/TuringCup/Faqs";
 
-import About from '../components/TuringCup/About';
+import About from "../components/TuringCup/About";
 
-
-
-export const Head = () => ( 
-    <Seo routename={"Turing Cup"}/>
-)
+export const Head = () => <Seo routename={"Turing Cup"} />;
 var divStyle = {
   margin: 0
-}
+};
 function turingcup() {
-return (
-//
-  
-  <Layout sx={{margin:0}} >
-    <div style={divStyle}>
-      <Box sx={{ backgroundColor: "#101820FF",color: "#F2AA4CFF" , fontFamily: "'Gruppo', cursive"}}>
-        <Landingpage2 style={{ margin:"0px",padding:"0px", fontFamily: "'Gruppo', cursive"}}/>
-        <About/>
-        <Timers/>
-        <Timelinee/>
-        <TuringCupPrizePool/>
-        <TuringCupSponsors/>
-        <PastContests/>
-        <Faqs />
-      </Box>
+  return (
+    //
+
+    <Layout sx={{ margin: 0 }}>
+      <div style={divStyle}>
+        <Box
+          sx={{
+            backgroundColor: "#101820FF",
+            color: "#F2AA4CFF",
+            fontFamily: "'Gruppo', cursive"
+          }}>
+          <Landingpage2
+            style={{
+              margin: "0px",
+              padding: "0px",
+              fontFamily: "'Gruppo', cursive"
+            }}
+          />
+          <About />
+          <Timers />
+          <Timelinee />
+          <TuringCupPrizePool />
+          <TuringCupSponsors />
+          <PastContests />
+          <Faqs />
+        </Box>
       </div>
-  </Layout>
-)
+    </Layout>
+  );
 }
 
-export default turingcup
+export default turingcup;
